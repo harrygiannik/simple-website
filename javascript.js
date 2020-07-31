9ed7829415a1f8b092cc0bb996043613fb0c617c
harrygiannik <harrygiannik@gmail.com>
2020-07-31 19:39:09 +0300

    topButton() fixed history.replace

--- javascript.js
+++ javascript.js
@@ -18,5 +18,5 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  history.replaceState(null, '', '/');
+  history.replaceState(null, '', './');
 }
